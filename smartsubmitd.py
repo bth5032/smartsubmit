6071192d75e5f9eb75fc8a4e3a32adaf319abdab
Bobak <bth5032@gmail.com>
2015-08-28 18:05:45 -0700

    added some more scaffolding to sqlite manager

--- smartsubmitd.py
+++ smartsubmitd.py
@@ -1,27 +1,51 @@
-import sys
+import sqlite3
+import os
 
 class SQLiteDBManager():
-"""A class which is meant to be a front end to the database for smartsubmit.
+	"""A class which is meant to be a front end to the database for smartsubmit.
 	The manager handles two tables. First, SampleFiles, which stores information
 	on where the sample files are stored. Second, Disks, which stores information
 	about which disks are available for storing the sample files."""
 
-	def __init__(self, DBPath):
-		self.connection=sqlite3.connect(DBPath)
+	def __new__(classname, arg):
+		"""This method makes sure we're sent a valid connection before we construct the object"""
+		if isinstance(arg, sqlite3.Connection):
+			return super(SQLiteDBManager, classname).__new__(classname)
+		else:
+			print("You have not provided a valid sqlite3 connection, construction of object aborted.")
+			return None
+
+	def __init__(self, connection):
+		if isinstance(connection, sqlite3.Connection):
+			self.connection=connection
+		else:
+			print("A valid connection is required")
+
 		self.cursor = connection.cursor()
 
 	def __repr__(self):
 		self.cursor.execute(".tables")
-		output = "List of Tables: %s" self.cursor.fetchall()
+		output = "List of Tables: %s" % self.cursor.fetchall()
 
 	def addSampleToDB(self, sample, localPath, hadoopPath, machine, IOSlotID, connection):
 		cursor=connection.cursor()
-		return "Expecting a string for the sample name" if !isinstance(sample, basestring)
-		return "Expecting a string for the local path" if !isinstance(sample, basestring)
-		return "Expecting a string for the Hadoop path" if !isinstance(sample, basestring)
-		return "Expecting a string for the machine" if !isinstance(sample, basestring)
-		return "Expecting a string for the IOSlot Identifier" if !isinstance(sample, basestring)
 		
+		if not isinstance(sample, basestring):
+			print("Expecting a string for the sample name")
+			return None
+		if not isinstance(sample, basestring):
+			print("Expecting a string for the local path")
+			return None
+		if not isinstance(sample, basestring):
+			print("Expecting a string for the Hadoop path" )
+			return None
+		if not isinstance(sample, basestring):
+			print("Expecting a string for the machine")
+			return None
+		if not isinstance(sample, basestring):
+			print("Expecting a string for the IOSlot Identifier")
+			return None
+
 		query = "INSERT INTO SampleFiles VALUES('%s', '%s', '%s', '%s', '%s')" % (sample, hadoopPath, localPath, IOSlotID, machine)
 		
 		if cursor.execute(query):
@@ -31,3 +55,12 @@ class SQLiteDBManager():
 			return None
 
 	def absorbDirectory(self, hadoopPath):
+		try:
+			fileList = os.listdir(hadoopPath)
+		except FileNotFoundError:
+			print("Could not open directory %s, check that the path is typed correctly." % hadoopPath)
+		toBeAbsorbed=[sampleName for sampleName in fileList if ".root" in sample] #Make a list of files that have ".root" in the name...
+		
+		print("Here's the list of samples that would go in the database")
+		for infile in toBeAbsorbed:
+			print(infile)
