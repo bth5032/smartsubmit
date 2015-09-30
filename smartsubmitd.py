4294739f7289ecaea3d465d00cc64e911c4f2034
Bobak <bth5032@gmail.com>
2015-09-30 15:01:28 -0700

    Added server scaffolding

--- smartsubmitd.py
+++ smartsubmitd.py
@@ -1,4 +1,4 @@
-import sqlman, sqlite3, itertools, os, subprocess, sys
+import sqlman, sqlite3, itertools, os, subprocess, sys, zmq
 from custom_errors import RsyncError
 
 
@@ -173,9 +173,12 @@ def getBestDisk(sample_name):
 
 	return [ {"CondorID": x[2], "Machine" : str(x[3]), "LocalDirectory" :str(x[4])} for x in output ]
 
-def populateFromDisks():
-	"""Remakes the SampleFiles table by indexing the files in Disks table. The method assumes that files for sample "SampleName" are in agreement with the convention being located in LocalDirectory/SampleName/"""
-	for rec in man["Disks"]:
-		ssh_syntax="ssh %s -t ls -R %s" % (rec[2], rec[1])
-		out = os.system(ssh_syntax)
-		print("output: %s" % out)
+### Server Part 
+
+context = zmq.Context()
+port="8000"
+socket=context.socket(zmq.REP)
+socket.bind("tcp://*:%s" % port)
+
+message=socket.recv()
+socket.send_string(str(getBestDisk("ttbar_powheg_pythia8_25ns")))
