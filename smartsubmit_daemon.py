7ae529499f1adf0938e92185fc59884a6dcd2102
Bobak <bth5032@gmail.com>
2015-10-05 12:07:53 -0700

    cleaning up file structure

--- smartsubmit_daemon.py
+++ smartsubmit_daemon.py
@@ -6,5 +6,17 @@ port="8000"
 socket=context.socket(zmq.REP)
 socket.bind("tcp://*:%s" % port)
 
-message=socket.recv()
-socket.send_pyobj(ss)
+while True:
+	message=socket.recv()
+	if message[:18] == "absorb sample file":
+		socket.send_string("Absorbing Sample File")
+		print(message)
+	elif message[:23] == "absorb sample directory":
+		socket.send_string("Absorbing Sample Directory")
+		print(message)
+	elif message[:18] == "delete sample file":
+		socket.send_string("Deleting Sample File")
+		print(message)
+	elif message[:7] == "run job":
+		socket.send_string("Running Job On Sample")
+		print(message)
