d7869c20d1aeff00a837d6030075b07058c9cb53
Bobak Hashemi <bth5032@gmail.com>
2015-10-12 19:54:40 -0700

    code cleaning

--- smartsubmit_ctrl.py
+++ smartsubmit_ctrl.py
@@ -1,5 +1,57 @@
 import zmq, argparse, os, sys
 
+def buildCommand(args):
+
+	if args.absorb_sample:
+		command+="absorb sample "
+		if args.sample:
+			if args.directory:
+				command+="directory "+args.directory+" "
+			elif args.file:
+				command+="file "+args.file+" "
+			else:
+				print("You must specify a file or directory to absorb with -f or -d")
+				return ""
+
+			command+=args.sample[0] #There should not be more than one sample for absorbsion
+			if len(args.sample) > 1:
+				print("There should only be one sample name specified")
+				return ""
+		else:
+			print("You must specify at least one sample name")
+			return ""	
+
+	elif args.delete_sample:
+		command+="delete sample file "
+		if args.file:
+			command+=args.file+" "
+		else:
+			print("You must specify a hadoop path to the file you want to delete.")
+			return ""
+
+	elif args.run_job:
+		command+="run job "
+		
+		if args.template:
+			command+=args.template+" "
+		else:
+			command+="./condorFileTemplate "
+
+		if args.executable:
+			command+=args.executable+" "
+			if args.sample:
+				for sample in args.sample:
+					command+=sample+" "
+			else:
+				print("You must specify a sample to run over")
+				return ""
+		else:
+			print("You must specify an executable to run")
+			return ""
+	else:
+		print(args.help)
+		return ""
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument( "--absorb_sample", help="absorb a sample file or directory, must be used with either -d or -f and -s", action="store_true")
@@ -12,16 +64,22 @@ parser.add_argument("-e", "--executable", help="specify the path to the executab
 parser.add_argument("-o", "--output", help="specify the directory for the file which will contain output from smartsubmit, default is the working directory")
 parser.add_argument("-t", "--template", help="specify the location of the condor submit file, optional argument used with --run_job; default is ./condorFileTemplate")
 
-
 args=parser.parse_args()
 
+# Construct the command to send to the server.
+# --------------------------------------------------------------------
+command = buildCommand(args)
+
+if not command: #the user messed up if empty
+	sys.exit(1)
+
 # Set up connection
+# -----------------------------------------------------------------------
 port="7584"
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://127.0.0.1:%s" % port)
-
 
 # Determine the directory for output files
 # -------------------------------------------------------------------------
@@ -37,63 +95,6 @@ socket.send_string(output_dir)
 message = socket.recv_string()
 
 print(message)
-
-
-# Construct the command to send to the server.
-# --------------------------------------------------------------------
-command=""
-
-if args.absorb_sample:
-	command+="absorb sample "
-	if args.sample:
-		if args.directory:
-			command+="directory "+args.directory+" "
-		elif args.file:
-			command+="file "+args.file+" "
-		else:
-			print("You must specify a file or directory to absorb with -f or -d")
-			sys.exit(1)
-		command+=args.sample[0] #There should not be more than one sample for absorbsion
-		if len(args.sample) > 1:
-			print("There should only be one sample name specified")
-			sys.exit(1)
-	else:
-		print("You must specify at least one sample name")
-		sys.exit(1)	
-
-elif args.delete_sample:
-	command+="delete sample file "
-	if args.file:
-		command+=args.file+" "
-	else:
-		print("You must specify a hadoop path to the file you want to delete.")
-		sys.exit(1)
-
-elif args.run_job:
-	command+="run job "
-	
-	if args.template:
-		command+=args.template+" "
-	else:
-		command+="./condorFileTemplate "
-
-	if args.executable:
-		command+=args.executable+" "
-		if args.sample:
-			for sample in args.sample:
-				command+=sample+" "
-		else:
-			print("You must specify a sample to run over")
-			sys.exit(1)
-	else:
-		print("You must specify an executable to run")
-		sys.exit(1)
-
-if command:
-	print(command)
-else:
-	print(args.help)
-
 
 # Send command to the server
 # -----------------------------------------------------------------------
