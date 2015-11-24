6041b327b4c1d5601364060d3c6e9f978df1082f
Bobak <bth5032@gmail.com>
2015-11-23 16:12:10 -0800

    added error checking for email login password

--- smartsubmit_daemon.py
+++ smartsubmit_daemon.py
@@ -9,11 +9,9 @@ import smtplib
 import email.mime.text as mt
 import email.utils as eutils
 
-
 admins = [["Bobak Hashemi", "bthashemi@ucsd.edu"]]
 username = "bthashem"
 password = getpass.getpass()
-
 
 context = zmq.Context()
 port="7584"
@@ -91,135 +89,158 @@ def checkOnJob(jobID):
 
 	return reply
 
-JID=0 #Job ID
-job_files = {}
-
-disk_check=threading.Thread(name="disk_check", target=diskCheckHelper)
-disk_check.setDaemon(True)
-disk_check.start()
-
-while True:
-	
-	# Get output file
-	
-	command=socket.recv_pyobj()
-	logging.info("recieved command: '%s' from user %s" % (command.command, command.user))
-
-	#threadname=time.strftime("ss_output_for_job_at_%m-%d-%Y_%H:%M:%S")
-
-	#working_dir="/tmp/"
-	#outfile_name = working_dir+threadname 
-	#outfile=open(outfile_name, "w+")
-	#command.outfile = outfile
-	#command.outfile_name = outfile_name
-
-
-	if command.command == "add file":
+def run_server():
+	while True:
 		
-		try:
-			threadname=time.strftime("ss_%s" % JID +"_%m-%d-%Y_%H:%M:%S")
+		# Get output file
+		
+		command=socket.recv_pyobj()
+		logging.info("recieved command: '%s' from user %s" % (command.command, command.user))
+
+		#threadname=time.strftime("ss_output_for_job_at_%m-%d-%Y_%H:%M:%S")
+
+		#working_dir="/tmp/"
+		#outfile_name = working_dir+threadname 
+		#outfile=open(outfile_name, "w+")
+		#command.outfile = outfile
+		#command.outfile_name = outfile_name
+
+
+		if command.command == "add file":
 			
-			working_dir="/tmp/"
-			outfile_name = working_dir+threadname 
-			outfile=open(outfile_name, "w+")
-			job_files[JID] = outfile
+			try:
+				threadname=time.strftime("ss_%s" % JID +"_%m-%d-%Y_%H:%M:%S")
+				
+				working_dir="/tmp/"
+				outfile_name = working_dir+threadname 
+				outfile=open(outfile_name, "w+")
+				job_files[JID] = outfile
 
-			print("absorbing sample file '%s' under sample name '%s' for user'%s'" % (command.hdp_path, command.sample, command.user))
+				print("absorbing sample file '%s' under sample name '%s' for user'%s'" % (command.hdp_path, command.sample, command.user))
 
-			if ' ' in command.sample:
-				message = "There can not be a space in the sample name '%s'" % command.sample
+				if ' ' in command.sample:
+					message = "There can not be a space in the sample name '%s'" % command.sample
+					print(message)
+					logging.error(message)
+					socket.send_pyobj(message)
+				else:
+					tp.printer.add_thread(threadname, outfile)
+					t=threading.Thread(name=threadname, target=addFile, args=(command.sample, command.hdp_path, command.user))
+					tp.printer.add_thread(threadname, outfile)
+					
+					socket.send_pyobj("The file is being moved and added to the database, you can check the status by running ss_ctrl [-c, --check_job] %s" % str(JID))
+					JID+=1
+					
+					t.start()
+			except IndexError:
+				print("error parsing command '%s'" % message)
+				socket.send_pyobj("Error parsing command '%s' " % message)
+
+		elif command.command == "delete file":
+			try:
+				hadoop_path_to_file = command.hdp_path
+				print("deleting sample file '%s'" % hadoop_path_to_file)
+				message = ss.deleteSampleFile(hadoop_path_to_file, command.user)
 				print(message)
-				logging.error(message)
 				socket.send_pyobj(message)
-			else:
-				tp.printer.add_thread(threadname, outfile)
-				t=threading.Thread(name=threadname, target=addFile, args=(command.sample, command.hdp_path, command.user))
-				tp.printer.add_thread(threadname, outfile)
-				
-				socket.send_pyobj("The file is being moved and added to the database, you can check the status by running ss_ctrl [-c, --check_job] %s" % str(JID))
-				JID+=1
-				
-				t.start()
-		except IndexError:
-			print("error parsing command '%s'" % message)
-			socket.send_pyobj("Error parsing command '%s' " % message)
+			except Exception as err:
+				print("Error: %s" % err)
+				logging.error("There was an error running the delete %s" % err)
+				print("There was an error parsing the command")
+				socket.send_pyobj("Error parsing command '%s' " % str(command))
 
-	elif command.command == "delete file":
-		try:
-			hadoop_path_to_file = command.hdp_path
-			print("deleting sample file '%s'" % hadoop_path_to_file)
-			message = ss.deleteSampleFile(hadoop_path_to_file, command.user)
-			print(message)
-			socket.send_pyobj(message)
-		except Exception as err:
-			print("Error: %s" % err)
-			logging.error("There was an error running the delete %s" % err)
-			print("There was an error parsing the command")
-			socket.send_pyobj("Error parsing command '%s' " % str(command))
+		elif command.command == "add directory":
+			
+			try:
+				threadname=time.strftime("ss_%s" % JID +"_%m-%d-%Y_%H:%M:%S")
+					
+				working_dir="/tmp/"
+				outfile_name = working_dir+threadname 
+				outfile=open(outfile_name, "w+")
+				job_files[JID] = outfile
 
-	elif command.command == "add directory":
+				print("absorbing directory '%s' under sample name '%s' for user'%s'" % (command.dir, command.sample, command.user))
+				logging.info("absorbing directory '%s' under sample name '%s' for user'%s'" % (command.dir, command.sample, command.user))
+
+				if ' ' in command.sample:
+					message = "There can not be a space in the sample name %s" % command.sample
+					print(message)
+					logging.error(message)
+					socket.send_pyobj(message)
+				else:
+					tp.printer.add_thread(threadname, outfile)
+					t=threading.Thread(name=threadname, target=addDirectory, args=(command.dir, command.sample, command.user))
+					tp.printer.add_thread(threadname, outfile)
+					
+					socket.send_pyobj("The directory is being absorbed into the system and added to the database, you can check the status by running ss_ctrl [-c, --check_job] %s" % str(JID))
+					JID+=1
+					
+					t.start()
+			except Exception as err:
+				print("error parsing command '%s'" % message)
+				socket.send_pyobj("Error parsing command '%s' " % message)
 		
-		try:
-			threadname=time.strftime("ss_%s" % JID +"_%m-%d-%Y_%H:%M:%S")
-				
-			working_dir="/tmp/"
-			outfile_name = working_dir+threadname 
-			outfile=open(outfile_name, "w+")
-			job_files[JID] = outfile
-
-			print("absorbing directory '%s' under sample name '%s' for user'%s'" % (command.dir, command.sample, command.user))
-			logging.info("absorbing directory '%s' under sample name '%s' for user'%s'" % (command.dir, command.sample, command.user))
-
-			if ' ' in command.sample:
-				message = "There can not be a space in the sample name %s" % command.sample
-				print(message)
-				logging.error(message)
-				socket.send_pyobj(message)
-			else:
-				tp.printer.add_thread(threadname, outfile)
-				t=threading.Thread(name=threadname, target=addDirectory, args=(command.dir, command.sample, command.user))
-				tp.printer.add_thread(threadname, outfile)
-				
-				socket.send_pyobj("The directory is being absorbed into the system and added to the database, you can check the status by running ss_ctrl [-c, --check_job] %s" % str(JID))
-				JID+=1
-				
-				t.start()
-		except Exception as err:
-			print("error parsing command '%s'" % message)
-			socket.send_pyobj("Error parsing command '%s' " % message)
-	
-	elif command.command == "run job":
-		ret = {}
-		for sample_name in command.samples:
-			ret[sample_name] = ss.computeJob(sample_name, command.user)
+		elif command.command == "run job":
+			ret = {}
+			for sample_name in command.samples:
+				ret[sample_name] = ss.computeJob(sample_name, command.user)
+			
+			socket.send_pyobj(ret)
 		
-		socket.send_pyobj(ret)
-	
-	elif command.command == "list sample files":
-		socket.send_pyobj(ss.man["SampleFiles"])
-		
-	elif command.command == "check job":
-		output = checkOnJob(command.jid)
-		socket.send_pyobj(output)
-
-	elif command.command == "update file sample":
-		try:
-			output = ss.updateSampleName(command.hdp_path, command.new_name)
+		elif command.command == "list sample files":
+			socket.send_pyobj(ss.man["SampleFiles"])
+			
+		elif command.command == "check job":
+			output = checkOnJob(command.jid)
 			socket.send_pyobj(output)
-		except Exception as err:
-			socket.send_pyobj("There was an error\n------\n%s\n running the command %s" (err,str(command)))
-		
-	else:
-		logging.error("No action defined for '%s'" % command.command)
-		print("""Error, no action defined for message '%s', allowable actions are:
-	1. add file
-	2. add directory
-	3. delete file
-	4. run job
-	5. list sample files
-	6. report bad disk
-	7. check job""" % command.command)
 
-	print(command)
+		elif command.command == "update file sample":
+			try:
+				output = ss.updateSampleName(command.hdp_path, command.new_name)
+				socket.send_pyobj(output)
+			except Exception as err:
+				socket.send_pyobj("There was an error\n------\n%s\n running the command %s" (err,str(command)))
+			
+		else:
+			logging.error("No action defined for '%s'" % command.command)
+			print("""Error, no action defined for message '%s', allowable actions are:
+		1. add file
+		2. add directory
+		3. delete file
+		4. run job
+		5. list sample files
+		6. report bad disk
+		7. check job""" % command.command)
+
+		print(command)
+
+def checkPass():
+	server = smtplib.SMTP('smtp.ucsd.edu', 587)
+	server.starttls()
+	try:
+		server.login(username, password)
+		server.quit()
+		print("Connected to mail server successfully with provided login info. Please press Ctrl+z to suspend this process to the background, then run bg to start the process again.")
+		return True
+	except Exception as err:
+		server.quit()
+		print("Could not connect to mail server, shutting down")
+		return False
 
 
+
+
+password_accepted = checkPass()
+
+if password_accepted:
+	JID=0 #Job ID
+	job_files = {}
+
+	disk_check=threading.Thread(name="disk_check", target=diskCheckHelper)
+	disk_check.setDaemon(True)
+	disk_check.start()
+
+	run_server()
+
+else:
+	exit(1)
