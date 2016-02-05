c01f90c8068d20222d9db1adcf10c57e185bf3a5
Bobak <bth5032@gmail.com>
2016-02-05 15:48:11 -0800

    Added cronjob script

--- testing/cronjob.sh
+++ testing/cronjob.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-WC_PATH="/nfs-7/t2tas/test_executable"
-
-pushd /tmp/ss_testing/
+pushd /root/ss_testing/
 
 out=`timeout 15 ss_ctrl --list_samples -v5`
 
 if [[ "$?" -eq "0" ]]
 then
+	#We have recieved the list of files from smartsubmit
 	for line in `echo $out | sed -e "s/\[//g" -e "s/',//g" -e "s/u'//g" -e "s/'//g" -e "s/,//g" | tr ']' '\n' | sed -e "s/^ //g" -e "s/ /|/g"` 
 	do 
-		for f in `echo $line | cut -d "|" -f 7,4,3 | sed -e "s/\(.*\)|\(.*\)|\(.*\)/\3:\1\2/g"`
+		#each line is the row of the SampleFiles directory delimited by | 
+		for f in `echo $line | cut -d "|" -f 3,4,5,7 | sed -e "s/\(.*\)|\(.*\)|\(.*\)|\(.*\)/\4:\1\2:\3\2/g"`
 		do
+			#Each f is a list of  
 			date=`date`
 			machine=`echo $f | cut -d ":" -f1`
 			location=`echo $f | cut -d ":" -f2`
+			hdloc=`echo $f | cut -d ":" -f3`
 			#hdloc=`echo $f | cut -d ":" -f3`
 			ret=`ssh $machine "wc -l $location"`
 			lastcount=`sqlite3 filetest.db "SELECT WordCount FROM FileInfo WHERE HadoopPath='$hdloc'"`
@@ -24,15 +26,20 @@ then
 			elif [[ -z "$lastcount" ]]
 				sqlite3 filetest.db "INSERT HadoopPath, WordCount, LastCheckDate INTO FileInfo Values('$hdloc','$ret','$date')"
 			else
-				echo "There may be an issue with the file at hadoop location $hloc\
-				\
-				At $date, the wordcount was $ret, but in the database the wordcount is $lastcount." | mailx -s "Possible issue with Smartsubmit file" "bthashemi@ucsd.edu"
+				LastCheckDate=`sqlite3 filetest.db "SELECT LastCheckDate FROM FileInfo WHERE HadoopPath=$hdloc"`
+				echo "Issue with file at hadoop location $hloc\
+\
+------- Date -- Wordcount\
+TODAY : $date -- $ret \
+OLD   : $LastCheckDate $lastcount." | mailx -s "file error" "bthashemi@ucsd.edu"
 			fi
 		done
 	done
 else
-	#There was an error, we never a list of files from the server
-	
+	#There was an error, we never got a list of files from the server
+	date=`date "+%F_%H:%M:%S"`
+	echo "Timeout server response at $date, please restart the server manually." | mailx -s "Possible issue with Smartsubmit server" "bthashemi@ucsd.edu"
+	#nohup python /root/smartsubmit/smartsubmit_daemon.py & > "stdout_$date.output"
 fi
 
 popd
