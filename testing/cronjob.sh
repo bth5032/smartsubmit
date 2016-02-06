f6ab5d1c4f19bfa7fcf7927359ef0db4ae9b6ad8
Bobak <bth5032@gmail.com>
2016-02-05 17:59:21 -0800

    minor

--- testing/cronjob.sh
+++ testing/cronjob.sh
@@ -10,8 +10,15 @@ date
 Message=""
 Errors="false"
 
+if [[ ! -f "/root/ss_testing/weblog" ]]
+then
+	touch /root/ss_testing/weblog
+fi
+
 if [[ "$?" -eq 0 ]]
 then
+	date=`date`
+	echo "$date: Server is alive" >> /root/ss_testing/weblog
 	#We have recieved the list of files from smartsubmit
 	for line in `echo $out | sed -e "s/\[//g" -e "s/',//g" -e "s/u'//g" -e "s/'//g" -e "s/,//g" | tr ']' '\n' | sed -e "s/^ //g" -e "s/ /|/g"` 
 	do 
@@ -19,7 +26,6 @@ then
 		for f in `echo $line | cut -d "|" -f 3,4,5,7 | sed -e "s/\(.*\)|\(.*\)|\(.*\)|\(.*\)/\4:\1\2:\3\2/g"`
 		do
 			#Each f is a list of  
-			date=`date`
 			machine=`echo $f | cut -d ":" -f1`
 			location=`echo $f | cut -d ":" -f2`
 			hdloc=`echo $f | cut -d ":" -f3`
@@ -54,6 +60,7 @@ else
 	#There was an error, we never got a list of files from the server
 	Errors="true"
 	date=`date "+%F_%H:%M:%S"`
+	echo "$date: Server may be down."
 	Message+="TIMEOUT ERROR: Timeout server response at $date, please restart the server manually.\n======================"
 fi
 
