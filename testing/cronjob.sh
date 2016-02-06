cfc9e879d8107e2c533ee8890d3bebb428ddab26
Bobak Hashemi <bth5032@gmail.com>
2016-02-06 14:17:43 -0800

    Fixed loop logic in cronjob.sh

--- testing/cronjob.sh
+++ testing/cronjob.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 pushd /root/ss_testing/
-
-out=`timeout 15 /root/smartsubmit/smartsubmit_ctrl.py --list_samples -v5`
-
 pwd
 date
 
@@ -15,6 +12,8 @@ then
 	touch /root/ss_testing/weblog
 fi
 
+out=`timeout 15 /root/smartsubmit/smartsubmit_ctrl.py --list_samples -v5`
+
 if [[ "$?" -eq 0 ]]
 then
 	date=`date`
@@ -23,38 +22,36 @@ then
 	for line in `echo $out | sed -e "s/\[//g" -e "s/',//g" -e "s/u'//g" -e "s/'//g" -e "s/,//g" | tr ']' '\n' | sed -e "s/^ //g" -e "s/ /|/g"` 
 	do 
 		#each line is the row of the SampleFiles directory delimited by | 
-		for f in `echo $line | cut -d "|" -f 3,4,5,7 | sed -e "s/\(.*\)|\(.*\)|\(.*\)|\(.*\)/\4:\1\2:\3\2/g"`
-		do
-			#Each f is a list of  
-			machine=`echo $f | cut -d ":" -f1`
-			location=`echo $f | cut -d ":" -f2`
-			hdloc=`echo $f | cut -d ":" -f3`
-			echo "$machine:$location"
-			ssh_output=`ssh $machine "wc -l $location"`
-			ret=`echo "$ssh_output" | cut -d ' ' -f1`
-			lastcount=`sqlite3 filetest.db "SELECT WordCount FROM FileInfo WHERE HadoopPath='$hdloc'"`
-			if [[ "$?" -eq 1 ]]
-			then
-				Errors="true"
-				#The file did not exist....
-				Message+="FILE ERROR: The file at $machine:$location was not readable by wc. \n HadoopPath: $hdloc \n======================"
-			elif [[ "$lastcount" == "$ret" ]]
-			then
-				echo "Counts Equal"
-				sqlite3 filetest.db "UPDATE FileInfo SET LastCheckDate='$date' WHERE HadoopPath='$hdloc'"
-			elif [[ -z "$lastcount" ]]
-			then
-				echo "No last count, inserting"
-				sqlite3 filetest.db "INSERT INTO FileInfo(HadoopPath, WordCount, LastCheckDate) Values('$hdloc','$ret','$date')"
-			else
-				Errors="true"
-				echo "Counts don't agree"
-				LastCheckDate=`sqlite3 filetest.db "SELECT LastCheckDate FROM FileInfo WHERE HadoopPath='$hdloc'"`
-				echo "Last count $LastCountDate -- $lastcount"
-				echo "This count $ret"
-				Message+="COUNT ERROR: Issue with file at hadoop location $hloc \n  TODAY : $ret ==||== $LastCheckDate:  $lastcount.\n======================"
-			fi
-		done
+		f=`echo $line | cut -d "|" -f 3,4,5,7 | sed -e "s/\(.*\)|\(.*\)|\(.*\)|\(.*\)/\4:\1\2:\3\2/g"`
+		#Each f is a list of  
+		machine=`echo $f | cut -d ":" -f1`
+		location=`echo $f | cut -d ":" -f2`
+		hdloc=`echo $f | cut -d ":" -f3`
+		echo "$machine:$location"
+		ssh_output=`ssh $machine "wc -l $location"`
+		ret=`echo "$ssh_output" | cut -d ' ' -f1`
+		lastcount=`sqlite3 filetest.db "SELECT WordCount FROM FileInfo WHERE HadoopPath='$hdloc'"`
+		if [[ "$?" -eq 1 ]]
+		then
+			Errors="true"
+			#The file did not exist....
+			Message+="FILE ERROR: The file at $machine:$location was not readable by wc. \n HadoopPath: $hdloc \n======================"
+		elif [[ "$lastcount" == "$ret" ]]
+		then
+			echo "Counts Equal"
+			sqlite3 filetest.db "UPDATE FileInfo SET LastCheckDate='$date' WHERE HadoopPath='$hdloc'"
+		elif [[ -z "$lastcount" ]]
+		then
+			echo "No last count, inserting"
+			sqlite3 filetest.db "INSERT INTO FileInfo(HadoopPath, WordCount, LastCheckDate) Values('$hdloc','$ret','$date')"
+		else
+			Errors="true"
+			echo "Counts don't agree"
+			LastCheckDate=`sqlite3 filetest.db "SELECT LastCheckDate FROM FileInfo WHERE HadoopPath='$hdloc'"`
+			echo "Last count $LastCountDate -- $lastcount"
+			echo "This count $ret"
+			Message+="COUNT ERROR: Issue with file at hadoop location $hdloc \n  TODAY : $ret ==||== $LastCheckDate:  $lastcount.\n======================"
+		fi
 	done
 else
 	#There was an error, we never got a list of files from the server
