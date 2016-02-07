f676ef3d67eb9ede72bcd820d701a7049ea5bae5
Bobak <bth5032@gmail.com>
2016-02-06 19:32:13 -0800

    minor

--- web/webserver.py
+++ web/webserver.py
@@ -16,7 +16,7 @@ app=Flask(__name__)
 def checkAlive():
 	ps = subprocess.Popen("ps aux", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 	exit_code = ps.returncode
-	with str(ps.stdout.readline().decode('UTF-8').rstrip('\n')) as out:
+	while out = str(ps.stdout.readline().decode('UTF-8').rstrip('\n'))
 		if "smartsubmit_daemon.py" in out:
 			return True
 	
