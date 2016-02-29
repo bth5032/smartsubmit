406347bceba924654ffc9b83ddd5cd208c273de4
Bobak <bth5032@gmail.com>
2016-02-29 11:00:35 -0800

    minor bug

--- testing/timer.py
+++ testing/timer.py
@@ -12,7 +12,7 @@ def getClusterIds():
 	"""Reads the standard input which should be filled with all the cluster ids for the jobs just submitted."""
 	ids = []
 	for line in sys.stdin:
-		if line:
+		if line.strip():
 			print(line[:-1])
 			ids.append(line[:-1])
 	return ids
